114ac3a0c4ad462233a6d4b531883702fa18710e
Emiliano <emadrigalce@gmail.com>
2024-07-07 00:59:31 -0600

    add database connection

---
+++ server/src/api/endpoints.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const {login} = require('../controllers/loginController');
+
+router.get('/login', login);
+
+module.exports = router
