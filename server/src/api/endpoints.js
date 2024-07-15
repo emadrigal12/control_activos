9bbdb59c185e9184956ef99c1b856bc6c837b58a
Emiliano <emadrigalce@gmail.com>
2024-07-15 15:18:39 -0600

    add login endpoint

--- server/src/api/endpoints.js
+++ server/src/api/endpoints.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {login} = require('../controllers/loginController');
 
-router.get('/login', login);
+router.post('/login', login);
 
 module.exports = router
